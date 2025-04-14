60b9e70e644301d6117ed3df8e9b123d626bca20
ALEJANDROCT777 <162204200+ALEJANDROCT777@users.noreply.github.com>
2025-04-13 22:42:42 -0600

    Update ACT3.py

--- ACT3.py
+++ ACT3.py
@@ -1,18 +1,11 @@
 
 import os
-
 import subprocess
 
- 
-
 def run(cmd):
-
     print(f"Ejecutando: {cmd}")
-
     subprocess.run(cmd, shell=True, check=True)
-
  
-
 def instalar_apache():
 
     # Detectar distro para usar el gestor correcto
@@ -21,7 +14,6 @@ def instalar_apache():
     if os.path.exists("/etc/os-release"):
         with open("/etc/os-release") as f:
             os_release = f.read()
-
     if "debian" in os_release or "ubuntu" in os_release:
         run("sudo apt update")
         run("sudo apt install -y apache2")
@@ -32,46 +24,26 @@ def instalar_apache():
         run("sudo yum install -y httpd")
         run("sudo systemctl start httpd")
         run("sudo systemctl enable httpd")
-
     else:
         print("Distro no soportada")
  
-
 def crear_pagina():
 
     html = """
-
     <!DOCTYPE html>
-
     <html lang="es">
-
     <head>
-
         <meta charset="UTF-8">
-
         <title>DevOps</title>
-
     </head>
-
     <body>
-
         <h1>¡Hola, bienvenido a el script de alejandro torres!</h1>
-
     </body>
-
     </html>
-
     """
-
     with open("/tmp/index.html", "w", encoding="utf-8") as f:
-
         f.write(html)
-
     run("sudo mv /tmp/index.html /var/www/html/index.html")
-
 if __name__ == "__main__":
-
     instalar_apache()
-
     crear_pagina()
-
